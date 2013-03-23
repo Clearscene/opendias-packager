87e05435ee96a2a31baf2417ce273a12a5ee9190
wbooth <wbooth@PVR.(none)>
2013-03-23 16:31:20 +0000

    futher redhat build fixes.

--- build.sh
+++ build.sh
@@ -105,7 +105,8 @@ packagerpm() {
       mkdir -p redhat/opendias/${DIR}
     done
     redhat/rules
-    rpmbuild --buildroot=${PWD}'/redhat/opendias' -bb --target i386 'redhat/opendias.spec'
+    #rpmbuild --buildroot=${PWD}'/redhat/opendias' -bb --target i386 'redhat/opendias.spec'
+    rpmbuild --buildroot=${PWD}'/redhat/opendias' -bb 'redhat/opendias.spec'
   else 
     echo Building an RPM is not supported in this platform
     exit 0
